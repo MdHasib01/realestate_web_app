124e16ce94a52aab0842051869634b89bae1c289
MdHasib01 <md.hasibuzzaman001@gmail.com>
2024-12-26 10:40:50 +0600

    change the property endpoint stracture

--- src/lib/store/features/property/propertyThunks.js
+++ src/lib/store/features/property/propertyThunks.js
@@ -20,7 +20,7 @@ export const getAllProperty = createAsyncThunk(
           headers: { Authorization: `Bearer ${accessToken}` },
         }
       );
-      return response.data.data.docs;
+      return response.data.data;
     } catch (error) {
       NextResponse.redirect(homeUrl);
       return rejectWithValue(error.response?.data || "Fetch failed");
