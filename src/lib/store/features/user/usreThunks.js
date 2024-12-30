f1672b1f04620298bb41da67f3aa8ccb583596d5
MdHasib01 <md.hasibuzzaman001@gmail.com>
2024-12-31 03:38:41 +0600

    fix the getAllUser thunk

--- src/lib/store/features/user/usreThunks.js
+++ src/lib/store/features/user/usreThunks.js
@@ -1,13 +1,19 @@
 "use client";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
+const accessToken =
+  typeof window !== "undefined"
+    ? window.localStorage.getItem("accessToken")
+    : "";
 export const getAllUsers = createAsyncThunk(
   "user/getAllUsers",
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/users/`
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/users/`,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
       );
       return response.data.data;
     } catch (error) {
