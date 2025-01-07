d9d05886e7d3f6e1eaa0f5d8fb1c78a829f47cf6
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-01-07 14:06:13 +0600

    complete the explore city section

--- src/app/(main)/city/[name]/page.jsx
+++ src/app/(main)/city/[name]/page.jsx
@@ -19,7 +19,8 @@ import { getQueryProperty } from "@/lib/store/features/property/propertyThunks";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { FaAngleRight } from "react-icons/fa";
+import PropertyCardLandscape from "@/components/FeaturedProperty/PropertyCardLandscape";
 const page = ({ params }) => {
   const [loading, setLoading] = useState(true);
   const { properties, isLoading, type, status, search, city } = useSelector(
@@ -50,7 +51,7 @@ const page = ({ params }) => {
         <CitySearch division={params?.name} setLoading={setLoading} />
       </div>
       <div className="container-main">
-        <Breadcrumb>
+        <Breadcrumb className="my-4">
           <BreadcrumbList>
             <BreadcrumbItem>
               <Link href="/">Home</Link>
@@ -61,7 +62,7 @@ const page = ({ params }) => {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-        <h2 className="text-3xl font-bold">{params?.name}</h2>
+        <h2 className="text-3xl font-bold mt-2">{params?.name}</h2>
         <div className="grid gird-cols-1 md:grid-cols-3 gap-4 my-8">
           <div className="grid md:col-span-2 grid-cols-1 md:grid-cols-2 gap-4">
             {loading
@@ -82,7 +83,118 @@ const page = ({ params }) => {
               </h2>
             )}
           </div>
-          <div className="col-span-1 bg-white rounded"></div>
+          <div className="col-span-1">
+            <div className="bg-white rounded p-6 mb-4">
+              <h2 className="text-2xl font-bold mb-4">Property Type</h2>
+              <ul>
+                <li className="flex items-center text-blue-500 font-semibold gap-1">
+                  <FaAngleRight />
+                  Commercial
+                </li>
+                <div className="pl-6">
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("office"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Office
+                  </li>
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("shop"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Shop
+                  </li>
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("warehouse"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Warehouse
+                  </li>
+                </div>
+              </ul>
+              <ul className="mt-4">
+                <li className="flex items-center text-blue-500 font-semibold gap-1">
+                  <FaAngleRight />
+                  Residential
+                </li>
+                <div className="pl-6">
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("apartment"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Apartment
+                  </li>
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("house"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    House
+                  </li>
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("studio"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Studio
+                  </li>
+                  <li
+                    className="flex items-center text-blue-500 font-semibold gap-1 cursor-pointer hover:text-blue-600 hover:translate-x-1 duration-300"
+                    onClick={() => {
+                      dispatch(setType("villa"));
+                      dispatch(getQueryProperty(searchParams));
+                    }}
+                  >
+                    <FaAngleRight />
+                    Villa
+                  </li>
+                </div>
+              </ul>
+            </div>
+            <div className="bg-white rounded p-6">
+              <h2 className="text-2xl font-bold mb-4">Cities</h2>
+              <ul>
+                <li className="flex items-center text-blue-500 font-semibold gap-1 hover:text-blue-600 hover:translate-x-1 duration-300">
+                  <FaAngleRight />
+                  <Link href="/city/Dhaka">Dhaka</Link>
+                </li>
+                <li className="flex items-center text-blue-500 font-semibold gap-1 hover:text-blue-600 hover:translate-x-1 duration-300">
+                  <FaAngleRight />
+                  <Link href="/city/Khulna">Khula</Link>
+                </li>
+                <li className="flex items-center text-blue-500 font-semibold gap-1 hover:text-blue-600 hover:translate-x-1 duration-300">
+                  <FaAngleRight />
+                  <Link href="/city/Chittagong">Chittagong</Link>
+                </li>
+                <li className="flex items-center text-blue-500 font-semibold gap-1 hover:text-blue-600 hover:translate-x-1 duration-300">
+                  <FaAngleRight />
+                  <Link href="/city/Rajshahi">Rajshahi</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/FeaturedProperty/PropertyCardLandscape.jsx
@@ -0,0 +1,157 @@
+"use client";
+import React from "react";
+import image from "../../app/assets/image/luxury-homes1.jpg";
+import Image from "next/image";
+import { GrExpand } from "react-icons/gr";
+import { IoBedOutline } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { GoHeart } from "react-icons/go";
+import { FiPlusCircle } from "react-icons/fi";
+import { FaShower } from "react-icons/fa6";
+import { FaCar } from "react-icons/fa";
+import { TbRulerMeasure } from "react-icons/tb";
+import { CgProfile } from "react-icons/cg";
+import { ImAttachment } from "react-icons/im";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import Link from "next/link";
+import getRelativeTime from "../../../utils/getRelativeTime";
+import formatDate from "../../../utils/formatDate";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleFavorite } from "@/lib/store/features/favourites/favouritesSlice";
+
+const PropertyCardLandscape = ({ property }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
+
+  const isFavorite = favorites.includes(property._id);
+  const handleClick = () => {
+    dispatch(toggleFavorite(property._id));
+  };
+  return (
+    <div className="bg-white flex rounded-lg border  relative">
+      <div className=" overflow-hidden w-1/3 rounded-l relative">
+        <img
+          className="bg-gray-500 object-cover w-full rounded-t-lg hover:scale-105 ease-in-out duration-500"
+          src={property?.image[0]}
+          alt="home"
+        ></img>
+        <div className="absolute bottom-2 right-2 flex gap-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="bg-[#00000084] text-white rounded text-lg hover:bg-[#00000599] p-1">
+                <GrExpand />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Preview</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger
+                onClick={handleClick}
+                className={`bg-[#00000084] ${
+                  isFavorite ? "text-red-500" : "text-white"
+                } rounded text-lg hover:bg-[#00000599] hover:text-red-500 p-1`}
+              >
+                <GoHeart />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Favorite</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="bg-[#00000084] text-white rounded text-lg hover:bg-[#00000599] p-1">
+                <FiPlusCircle />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Compare</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+
+      <div>
+        <div className="p-4">
+          <Link href={`/property/${property?._id}`}>
+            <h2 className="text-xl hover:text-blue-500 duration-300 font-bold">
+              {property.title}
+            </h2>
+          </Link>
+          {property?.location && (
+            <p className="text-blue-500 underline cursor-pointer my-2 flex items-center gap-2">
+              <FaLocationDot className="text-blue-500" />
+              {property?.location}, {property?.city}, {property?.divission}
+            </p>
+          )}
+          {property?.createdAt && (
+            <p className="text-sm text-gray-500 flex">
+              <span className="text-black">Added:</span>
+              <p>{formatDate(property?.createdAt)}</p>
+            </p>
+          )}
+          <div className="flex items-center gap-4 my-2 text-sm">
+            {property?.bedrooms && (
+              <div className="flex items-center gap-1 ">
+                <IoBedOutline className="text-xl" /> {property?.bedrooms}
+              </div>
+            )}
+            {property?.bathrooms && (
+              <div className="flex items-center gap-1 text-sm">
+                <FaShower className="text-xl" /> {property?.bathrooms}
+              </div>
+            )}
+            {property?.garage && (
+              <div className="flex items-center gap-1 text-sm">
+                <FaCar className="text-xl" /> {property?.garage}
+              </div>
+            )}
+            {property?.size && (
+              <div className="flex items-center gap-1 text-sm">
+                <TbRulerMeasure className="text-xl" /> {property?.size} Sq Ft
+              </div>
+            )}
+          </div>
+          <p className="text-xs uppercase ">APARTMENT</p>
+        </div>
+        <hr />
+        <div className="flex justify-between text-gray-700 items-center p-4">
+          {property?.ownerDetails && (
+            <div className="flex items-center gap-2 text-sm">
+              {property?.ownerDetails?.avatar ? (
+                <img
+                  src={property?.ownerDetails?.avatar}
+                  className="w-8 h-8 rounded rounded-full p-1 border"
+                  alt="profile"
+                />
+              ) : (
+                <CgProfile className="text-xl" />
+              )}
+              <p>{property?.ownerDetails?.fullName}</p>
+            </div>
+          )}
+          <div className="flex items-center gap-1 text-sm">
+            <ImAttachment className="text-xl" />
+            <p>{getRelativeTime(property?.createdAt || new Date())}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyCardLandscape;
+//  <div className="absolute top-2 left-2 bg-green-500 text-white rounded text-xs px-2 py-1">
+//         Featured
+//       </div>
+//       <div className="absolute top-2 right-2 bg-[#00000084] text-white rounded text-xs px-2 py-1">
+//         For Rent
+//       </div>
